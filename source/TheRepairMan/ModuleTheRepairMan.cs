bbf621c2b0cc25381f982265697072dbbdeb9572
Aelfhe1m <aelfhe1m@hotmail.com>
2017-05-19 16:57:47 +0100

    Initial project and part module mock-up

---
+++ source/TheRepairMan/ModuleTheRepairMan.cs
@@ -0,0 +1,29 @@
+﻿namespace Aelfhe1m.TheRepairMan
+{
+    public class ModuleTheRepairMan : PartModule
+    {
+        [KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Do Repairs")]
+        public void DoRepairs()
+        {
+            var callingPart = this.part;
+
+        }
+
+        public void FixedUpdate()
+        {
+            if (HighLogic.LoadedScene == GameScenes.EDITOR)
+                return;
+
+            // do check for repairable parts
+            var repairableParts = false;
+            if (repairableParts)
+            {
+                Events["DoRepairs"].active = false;
+            }
+            else
+            {
+                Events["DoRepairs"].active = true;
+            }
+        }
+    }
+}
