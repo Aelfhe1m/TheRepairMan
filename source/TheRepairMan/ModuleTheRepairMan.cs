f32c951070996847cc05e32ef7713a2123b1e093
Aelfhe1m <aelfhe1m@hotmail.com>
2017-05-19 17:14:21 +0100

    EVA engineer only code. Add MM patch

--- source/TheRepairMan/ModuleTheRepairMan.cs
+++ source/TheRepairMan/ModuleTheRepairMan.cs
@@ -2,11 +2,33 @@
 {
     public class ModuleTheRepairMan : PartModule
     {
-        [KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Do Repairs")]
+        [KSPField]
+        public float EVARange = 5f;
+
+        [KSPField]
+        public string Menu = "Do Repairs";
+
+        [KSPEvent(active = true, guiActiveUnfocused = true, externalToEVAOnly = true, guiName = "Do Repairs", unfocusedRange = 5f)]
         public void DoRepairs()
         {
-            var callingPart = this.part;
+            var kerbal = FlightGlobals.ActiveVessel.rootPart.protoModuleCrew[0];
+            if (!kerbal.HasEffect("RepairSkill"))
+            {
+                ScreenMessages.PostScreenMessage("Only Kerbals with repair skills can do repairs!", 5f, ScreenMessageStyle.UPPER_CENTER);
+                return;
+            }
 
+            UnityEngine.Debug.Log("[A_TRM]: Do repairs");
+            ScreenMessages.PostScreenMessage("Doing repairs", 5f, ScreenMessageStyle.UPPER_CENTER);
+
+        }
+
+        public override void OnStart(StartState state)
+        {
+            // make range and menu action name configurable
+            Events["DoRepairs"].unfocusedRange = EVARange;
+            Events["DoRepairs"].guiName = Menu;
+            base.OnStart(state);
         }
 
         public void FixedUpdate()
