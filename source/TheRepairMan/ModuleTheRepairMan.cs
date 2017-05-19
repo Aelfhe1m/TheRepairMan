9b843382b20b140baf3ede29b9353d50db472102
Aelfhe1m <aelfhe1m@hotmail.com>
2017-05-19 18:25:36 +0100

    Add configurable minimum experience levels required for repairs

--- source/TheRepairMan/ModuleTheRepairMan.cs
+++ source/TheRepairMan/ModuleTheRepairMan.cs
@@ -1,4 +1,6 @@
-﻿namespace Aelfhe1m.TheRepairMan
+﻿using System.Collections.Generic;
+
+namespace Aelfhe1m.TheRepairMan
 {
     public class ModuleTheRepairMan : PartModule
     {
@@ -7,6 +9,15 @@
 
         [KSPField]
         public string Menu = "Do Repairs";
+
+        [KSPField]
+        public int MinSolarPanelRepairLevel = 2;
+
+        [KSPField]
+        public int MinAntennaeRepairLevel = 1;
+
+        [KSPField]
+        public int MinRadiatorRepairLevel = 3;
 
         [KSPEvent(active = true, guiActiveUnfocused = true, externalToEVAOnly = true, guiName = "Do Repairs", unfocusedRange = 5f)]
         public void DoRepairs()
@@ -18,8 +29,27 @@
                 return;
             }
 
-            UnityEngine.Debug.Log("[A_TRM]: Do repairs");
-            ScreenMessages.PostScreenMessage("Doing repairs", 5f, ScreenMessageStyle.UPPER_CENTER);
+            var repairables = FindRepairables();
+
+            if (repairables.Count > 0)
+            {
+                var repaired = RepairAntennae(kerbal, repairables);
+                if (repaired > 0)
+                    ScreenMessages.PostScreenMessage($"Repaired {repaired} antennae", 5f, ScreenMessageStyle.UPPER_CENTER);
+
+                repaired = RepairSolarPanels(kerbal, repairables);
+                if (repaired > 0)
+                    ScreenMessages.PostScreenMessage($"Repaired {repaired} solar panels", 5f, ScreenMessageStyle.UPPER_CENTER);
+
+                repaired = RepairRadiators(kerbal, repairables);
+                if (repaired > 0)
+                    ScreenMessages.PostScreenMessage($"Repaired {repaired} radiators", 5f, ScreenMessageStyle.UPPER_CENTER);
+
+            }
+            else
+            {
+                ScreenMessages.PostScreenMessage("Nothing to repair", 5f, ScreenMessageStyle.UPPER_CENTER);
+            }
 
         }
 
@@ -31,20 +61,70 @@
             base.OnStart(state);
         }
 
+        public Repairables FindRepairables()
+        {
+            var found = new Repairables();
+
+            found.Antennae = this.vessel.FindPartModulesImplementing<ModuleDeployableAntenna>();
+            found.SolarPanels = this.vessel.FindPartModulesImplementing<ModuleDeployableSolarPanel>();
+            found.Radiators = this.vessel.FindPartModulesImplementing<ModuleDeployableRadiator>();
+
+            return found;
+        }
+
+        public int RepairAntennae(ProtoCrewMember kerbal, Repairables repairables)
+        {
+            if (kerbal.experienceLevel > MinAntennaeRepairLevel)
+            {
+                return repairables.Antennae.Count;
+            }
+            else
+            {
+                ScreenMessages.PostScreenMessage($"Engineer must be at least level {MinAntennaeRepairLevel} to repair antennae.", 5f, ScreenMessageStyle.UPPER_CENTER);
+                return 0;
+            }
+        }
+
+        public int RepairSolarPanels(ProtoCrewMember kerbal, Repairables repairables)
+        {
+            if (kerbal.experienceLevel > MinSolarPanelRepairLevel)
+            {
+                return repairables.SolarPanels.Count;
+            }
+            else
+            {
+                ScreenMessages.PostScreenMessage($"Engineer must be at least level {MinSolarPanelRepairLevel} to repair solar panels.", 5f, ScreenMessageStyle.UPPER_CENTER);
+                return 0;
+            }
+        }
+
+        public int RepairRadiators(ProtoCrewMember kerbal, Repairables repairables)
+        {
+            if (kerbal.experienceLevel > MinRadiatorRepairLevel)
+            {
+                return repairables.Radiators.Count;
+            }
+            else
+            {
+                ScreenMessages.PostScreenMessage($"Engineer must be at least level {MinRadiatorRepairLevel} to repair radiators.", 5f, ScreenMessageStyle.UPPER_CENTER);
+                return 0;
+            }
+        }
+
         public void FixedUpdate()
         {
             if (HighLogic.LoadedScene == GameScenes.EDITOR)
                 return;
 
             // do check for repairable parts
-            var repairableParts = false;
-            if (repairableParts)
+            var repairables = FindRepairables();
+            if (repairables.Count > 0)
             {
-                Events["DoRepairs"].active = false;
+                Events["DoRepairs"].active = true;
             }
             else
             {
-                Events["DoRepairs"].active = true;
+                Events["DoRepairs"].active = false;
             }
         }
     }
---
+++ source/TheRepairMan/Repairables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aelfhe1m.TheRepairMan
+{
+    public class Repairables
+    {
+        public List<ModuleDeployableAntenna> Antennae = new List<ModuleDeployableAntenna>();
+        public List<ModuleDeployableSolarPanel> SolarPanels = new List<ModuleDeployableSolarPanel>();
+        public List<ModuleDeployableRadiator> Radiators = new List<ModuleDeployableRadiator>();
+
+        public int Count
+        {
+            get
+            {
+                return Antennae.Count + SolarPanels.Count + Radiators.Count;
+            }
+        }
+    }
+}
